ce1f09acecee57f38a8932eae43f13ad206b4e84
ViachDiach <105353544+ViachDiach@users.noreply.github.com>
2023-08-30 20:07:44 +0200

    one more

--- Program.cs
+++ Program.cs
@@ -40,21 +40,19 @@ namespace MyFirstBot
             slashCommands.RegisterCommands<StuffyCommands>();
             slashCommands.RegisterCommands<SwapCommands>();
             
-            // discord.GuildAvailable  += async (client, args) =>
-            // {
-            //     foreach (var guild  in client.Guilds.Values)
-            //     {
-            //         var textChannels = guild.Channels.Values.Where(channel => channel.Type == ChannelType.Text);
+            discord.GuildAvailable  += async (client, args) =>
+            {
+                foreach (var guild  in client.Guilds.Values)
+                {
+                    var textChannels = guild.Channels.Values.Where(channel => channel.Type == ChannelType.Text);
 
-            //         foreach (var channel in textChannels)
-            //         {
-            //             await  channel.SendMessageAsync("Доброго времени суток");
-            //         }
-            //     }
-            // };
-
-            Console.WriteLine("Hello world");
-
+                    foreach (var channel in textChannels)
+                    {
+                        await  channel.SendMessageAsync("Доброго времени суток");
+                    }
+                }
+            };
+            
             await discord.ConnectAsync();
             await Task.Delay(-1);
         }
