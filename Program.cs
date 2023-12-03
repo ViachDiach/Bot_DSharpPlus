8e2a9c0ca00bc5c4cdf5e1dfbe07b56e32713e84
ViachDiach <105353544+ViachDiach@users.noreply.github.com>
2023-12-03 22:36:05 +0100

    Добавил обработчик сотыбия подключение-отключение пользователя

--- Program.cs
+++ Program.cs
@@ -3,9 +3,10 @@ using DSharpPlus.SlashCommands;
 using static DSharpAPP.commands.Program;
 using DSharpAPP.db;
 using Newtonsoft.Json; 
+using DSharpPlus.Entities;
 using DSharpPlus.Lavalink;
 using DSharpPlus.Net;
-// using System.Diagnostics;
+using DSharpPlus.EventArgs;
 
 namespace MyFirstBot
 {
@@ -15,30 +16,30 @@ namespace MyFirstBot
         {
             public string Token { get; set; }
         }
+        
         static async Task Main(string[] args)
         {
             var configPath = "Config.json";
             var configJson = File.ReadAllText(configPath);
             var config = JsonConvert.DeserializeObject<Config>(configJson);
 
-            // string cmdPath = "D:\\VSC+Net\\LavalinkServer";
-            // string cmdArgument = $"/C java -jar \"D:\\VSC+Net\\LavalinkServer\\Lavalink.jar\"";
-
             var discord = new DiscordClient(new DiscordConfiguration()
             {
                 Token = config.Token, // Используем токен из конфигурации
                 TokenType = TokenType.Bot,
-                Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents
+                Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents | DiscordIntents.GuildVoiceStates,
+                
+                
             });
 
-            // Process process = new Process();
-            // process.StartInfo.FileName = "cmd.exe"; 
-            // process.StartInfo.Arguments = cmdArgument;
-            // process.StartInfo.WorkingDirectory = cmdPath;
-            // process.Start();
+            discord.GuildDownloadCompleted  += OnGuildDownloadCompleted;
+            discord.ComponentInteractionCreated += ButtonPressResponce;
+            discord.VoiceStateUpdated += Voice_State_Updated; 
+
 
             var endpoint = new ConnectionEndpoint
             {
+                // Hostname = "90.188.250.219", // From your server configuration.
                 Hostname = "127.0.0.1", // From your server configuration.
                 Port = 80 // From your server configuration
             };
@@ -47,13 +48,13 @@ namespace MyFirstBot
             // Password = "secret_password", // From your server configuration.
             var lavalinkConfig = new LavalinkConfiguration
             {
+                // Password = "secret_password", // From your server configuration.
                 Password = "youshallnotpass", // From your server configuration.
                 RestEndpoint = endpoint,
                 SocketEndpoint = endpoint
             };
 
             var lavalink = discord.UseLavalink();
-
             SQL.init(); // Инициализация подключения к базе данных
 
             var slashCommands = discord.UseSlashCommands(new SlashCommandsConfiguration
@@ -74,7 +75,52 @@ namespace MyFirstBot
             await discord.ConnectAsync();
             await lavalink.ConnectAsync(lavalinkConfig); // Make sure this is after Discord.ConnectAsync().
 
+            await DSharpAPP.commands.CheckConnection.SendReport(discord);
+            // await DSharpAPP.commands.CheckConnection.CreateAnswer(discord);
+
             await Task.Delay(-1);
+        }
+
+        private static async Task Voice_State_Updated(DiscordClient sender, VoiceStateUpdateEventArgs args)
+        {
+            var guild = args.Guild;
+            if (guild == null)
+                return;
+
+            var userId = args.User.Id;
+            var disconnectTime = DateTime.Now;
+
+            var member = await guild.GetMemberAsync(userId);
+            if (member == null)
+                return;
+
+            var voiceState = member.VoiceState;
+            if (voiceState == null)
+                return;
+
+            if (voiceState.Channel != null)
+            {
+                Console.WriteLine($"{member.DisplayName} AND {member.Id} has joined voice channel {voiceState.Channel.Name}");
+            }
+            else
+            {
+                Console.WriteLine($"{member.DisplayName} AND  has left voice channel at");
+            }
+        }
+
+        private static Task OnGuildDownloadCompleted(DiscordClient sender, GuildDownloadCompletedEventArgs args)
+        {
+            return Task.CompletedTask;
+        }
+
+        private static Task ButtonPressResponce(DiscordClient sender, DSharpPlus.EventArgs.ComponentInteractionCreateEventArgs e)
+        {
+            if (e.Interaction.Data.CustomId == "1") 
+            {
+                Console.WriteLine("You selected Option1");
+            }
+            
+            return Task.CompletedTask;
         }
 
         private static IServiceProvider ConfigureServices()
